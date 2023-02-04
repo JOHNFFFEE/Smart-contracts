566cc8391db7cf664038e26bb1a116c96cf9d311
JOHNFFFEE <yo.nitu@hotmail.com>
2023-02-04 20:48:09 +0200

    Create UseThemExample.sol

---
+++ tokenomics/Crowdsale/UseThemExample.sol
@@ -0,0 +1,22 @@
+
+// SPDX-License-Identifier: MIT
+
+           
+pragma solidity >=0.8.13 <0.9.0;
+
+    import "./Crowdsale.sol";
+    import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+   
+ contract MyCrowdsale is Crowdsale {
+    constructor(
+        uint256 rate,
+        address payable wallet,
+        IERC20 token
+    )  Crowdsale(rate, wallet, token){}
+
+    function _deliverTokens(address beneficiary, uint256 tokenAmount) internal override {
+        ERC20PresetMinterPauser(address(token())).mint(beneficiary, tokenAmount);
+}
+ 
+ }
+ 
