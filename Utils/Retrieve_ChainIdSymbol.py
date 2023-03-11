123ff5dbb8a6acbc7cd890707895ed4e77de10f9
JOHNFFFEE <yo.nitu@hotmail.com>
2023-03-12 00:54:02 +0200

    Update Retrieve_ChainIdSymbol.py

--- Utils/Retrieve_ChainIdSymbol.py
+++ Utils/Retrieve_ChainIdSymbol.py
@@ -9,19 +9,23 @@ response = requests.get(url)
 df = pd.read_json(response.content)
 
 # Select only the columns for name, chainId, and nativeCurrency
-df = df[["name", "chainId", "networkId", "nativeCurrency"]]
+df = df[["name", "chainId", "networkId", "nativeCurrency","rpc"]]
 
 # Extract the symbol from the nativeCurrency object
 df["symbol"] = df["nativeCurrency"].apply(lambda x: x["symbol"])
 
-# Select only the columns for chain, chainId, and symbol
-df = df[["name", "chainId", "networkId",  "symbol"]]
+# Extract the first RPC endpoint from the rpc list, if it exists
+df["rpcs"] = df["rpc"].apply(lambda x: x[0] if len(x) > 0 else None)
+
+# Select only the columns for chain, chainId, symbol, and rpcs
+df = df[["name", "chainId", "networkId", "symbol", "rpcs"]]
 
 # Rename columns to match your expected format
-df.columns = ["chain", "chainId", "networkId", "symbol"]
+df.columns = ["chain", "chainId", "networkId", "symbol", "rpc"]
 
 # Print the resulting DataFrame
 print(df)
+
 
 # Save the DataFrame to a JSON file
 df.to_json("./chains.json", orient="records")
