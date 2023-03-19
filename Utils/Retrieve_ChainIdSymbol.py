556491587ae6321aa4b8572a48456a91d70aeb55
JOHNFFFEE <yo.nitu@hotmail.com>
2023-03-19 22:56:03 +0200

    rpc change

--- Utils/Retrieve_ChainIdSymbol.py
+++ Utils/Retrieve_ChainIdSymbol.py
@@ -16,16 +16,20 @@ df = df[["name", "chainId", "networkId", "nativeCurrency","rpc", "explorers"]]
 # Extract the symbol from the nativeCurrency object
 df["symbol"] = df["nativeCurrency"].apply(lambda x: x["symbol"])
 
-df["url"] = df["explorers"].apply(lambda x: x[0]["url"] if isinstance(x, list) and len(x) > 0 else None)
+df["explorer_url"] = df["explorers"].apply(lambda x: x[0]["url"] if isinstance(x, list) and len(x) > 0 else None)
+
 
 # Extract the first RPC endpoint from the rpc list, if it exists
-df["rpcs"] = df["rpc"].apply(lambda x: x[0] if len(x) > 0 else None)
+# df["rpcs"] = df["rpc"].apply(lambda x: x[0] if len(x) > 0 else None)
+# dont take infura if there are many rpcs
+df["rpcs"] = df["rpc"].apply(lambda x: [r for r in x if "infura" not in r][0] if len(x) > 1 and any("infura" not in r for r in x) else x[0] if len(x) > 0 else None)
+
 
 # Select only the columns for chain, chainId, symbol, and rpcs
-df = df[["name", "chainId", "networkId", "symbol", "rpcs","url"]]
+df = df[["name", "chainId", "networkId", "symbol", "rpcs","explorer_url"]]
 
 # Rename columns to match your expected format
-df.columns = ["chain", "chainId", "networkId", "symbol", "rpc","url"]
+df.columns = ["chain", "chainId", "networkId", "symbol", "rpc","explorer_url"]
 
 # Print the resulting DataFrame
 print(df)
